a240b5a8b3b8ff70bb963e805b254219366b28d8
hatem <hatemazaiez1@gmail.com>
2024-01-13 22:54:48 +0100

    new commit

---
+++ contracts/TamkinToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TamkinToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Tamkin", "TSLT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function burn(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to burn");
+        _burn(msg.sender, amount);
+    }
+}
