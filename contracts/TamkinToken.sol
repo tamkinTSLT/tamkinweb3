442560cf23051745077404be101158ba1ca96cbf
hatem <hatemazaiez1@gmail.com>
2024-01-13 22:59:10 +0100

    new

--- contracts/TamkinToken.sol
+++ contracts/TamkinToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
 contract TamkinToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Tamkin", "TSLT") {
         _mint(msg.sender, initialSupply);
